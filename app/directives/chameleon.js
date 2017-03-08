50da235a5a73f7a7b25405f31f8a9d2abce6ae08
adam <alucia@communityelf.com>
2017-03-07 20:40:47 -0500

    init"

--- app/directives/chameleon.js
+++ app/directives/chameleon.js
@@ -6,10 +6,11 @@ angular.module('mySite').directive('chameleon', ['$window', function ($window) {
             color: "@"
         },
         link: function (scope, element, attrs) {
-            console.log('scope.color', scope.color);
+
             //create chameleonElement and put if after element that the directive is on
             var chameleonElement = angular.element(`<div id="${ element[0].id }-chameleon"></div>`);
 
+             //chameleon element height to directive element height
             chameleonElement.css('height', element[0].offsetHeight+'px');
             chameleonElement.css('position', 'relative');
             chameleonElement.css('top', '-'+element[0].offsetHeight+'px');
@@ -21,18 +22,28 @@ angular.module('mySite').directive('chameleon', ['$window', function ($window) {
             //console.log('chameleonElement', chameleonElement);
             element.after(chameleonElement);
             element.css('z-index', 100)
-            var elementsTopOffset = element.topOffset;
+
+
+            var elementOffsetHeight = element[0].offsetHeight;
+
+
 
             angular.element($window).bind("scroll", function() {
-                // console.log('this.pageYOffset', this.pageYOffset);
-                console.log('element[0].offsetTop', element[0].offsetTop/this.pageYOffset);
+                console.log('$window.pageYOffset', $window.pageYOffset);
 
-               if (this.pageYOffset >= parseInt(scope.offset)) {
-                   chameleonElement.css('background-color', `rgba(${scope.color}, ${element[0].offsetTop/this.pageYOffset})`);
-                   //element.css('opacity', element[0].offsetTop/this.pageYOffset)
-               } else {
-
-               }
+                console.log('element[0].pageYOffset', element[0].getBoundingClientRect().top);
+                // element[0].offsetHeight/$window.pageYOffset
+                console.log('element[0].offsetTop/$window.pageYOffset', element[0].offsetHeight/$window.pageYOffset);
+                //console.log('this.pageYOffset', element[0].offsetTop/$window.pageYOffset);
+                //console.log('element[0].offsetTop',`rgba(${scope.color}, ${element[0].offsetTop/this.pageYOffset})` );
+                chameleonElement.css('background-color', `rgba(${scope.colr}, ${element[0].offsetHeight/$window.pageYOffset})`);
+            //    if ($window.pageYOffset >= parseInt(scope.offset)) {
+            //        console.log('in here');
+            //        chameleonElement.css('background-color', `rgba(${scope.color}, ${$window.pageYOffset/element[0].offsetHeight})`);
+            //        //element.css('opacity', element[0].offsetTop/this.pageYOffset)
+            //    } else {
+               //
+            //    }
            });
         }
     }
