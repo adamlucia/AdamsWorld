26451c0bc2784a808960fcc784a2ab7065f818b8
adam <alucia@communityelf.com>
2017-03-14 15:34:15 -0400

    Localize slide directive in html, get slider highlighting working, adjust css

--- app/directives/slideHighlight.js
+++ app/directives/slideHighlight.js
@@ -5,20 +5,37 @@ angular.module('mySite').directive('slideHighlight', ['$window', function ($wind
             color: "@"
         },
         link: function (scope, element, attrs) {
-            console.log('scope.color', scope.color);
+            console.log('element', element);
 
             angular.element($window).bind("scroll", function() {
-                console.log('element[0].offsetTop', element[0].offsetTop);
-                //this.pageYOffset
 
-               if (this.pageYOffset >= parseInt(scope.offset)) {
-                   chameleonElement.css('background-color', `rgba(${scope.color}, ${element[0].offsetTop/this.pageYOffset})`);
-                   chameleonElement.css('background-color', `rgba(${scope.color}, ${1 - this.pageYOffset / (element[0].offsetHeight/2)})`);
-                   //element.css('opacity', element[0].offsetTop/this.pageYOffset)
-               } else {
-
-               }
+                if(isOnScreen(element)){
+                    console.log(element[0].innerHTML, isOnScreen(element));
+                    element.css('background-position', '0 0');
+                }
            });
+
+           isOnScreen = function(element){
+
+                var viewport = {
+                    top : $window.pageYOffset,
+                    left : $window.pageXOffset
+                };
+
+                viewport.right = viewport.left + $window.innerWidth;
+                viewport.bottom = viewport.top + $window.innerHeight;
+
+                console.log('viewport', viewport);
+                var bounds = {
+                    top : this.pageYOffset,
+                    left: this.pageXOffset
+                };
+                bounds = element[0].getBoundingClientRect();
+
+                //+200 to slide when in view
+                return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < (bounds.top + 200) || viewport.top > bounds.bottom));
+            };
+
         }
     }
 }]);
