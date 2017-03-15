3cf470f843de04069138a02a387bd32fabcaea70
adam <alucia@communityelf.com>
2017-03-15 19:08:34 -0400

    update slide highlight to update only background image to fix px at end of element

--- app/directives/slideHighlight.js
+++ app/directives/slideHighlight.js
@@ -2,38 +2,38 @@ angular.module('mySite').directive('slideHighlight', ['$window', function ($wind
     return {
         restrict: 'A',
         scope: {
-            color: "@"
         },
         link: function (scope, element, attrs) {
-            console.log('element', element);
+
+            element.css('margin', '0px 0px 0px -20px');
+            element.css('display', 'inline-block');
+            element.css('background-image', 'linear-gradient(to left, transparent, transparent 50%, #3bc7f9 50%, #3bc7f9)');
+            element.css('background-position', '100% 0');
+            element.css('background-size', '0% 100%');
+            element.css('-webkit-transition', 'background-position .5s ease-in');
+            element.css('-moz-transition', 'background-position .5s ease-in');
+            element.css('-ms-transition', 'background-position .5s ease-in');
+        	element.css('-o-transition', 'background-position .5s ease-in');
+            element.css('transition', 'background-position .5s ease-in');
+
+            var viewport = {
+                top : $window.pageYOffset,
+                left : $window.pageXOffset
+            };
+            viewport.right = viewport.left + $window.innerWidth;
+            viewport.bottom = viewport.top + $window.innerHeight;
 
             angular.element($window).bind("scroll", function() {
-
-                if(isOnScreen(element)){
-                    console.log(element[0].innerHTML, isOnScreen(element));
+                if(isOnScreen(viewport, element)){
+                    element.css('background-size', '200% 100%');
                     element.css('background-position', '0 0');
+
                 }
-           });
+            });
 
-           isOnScreen = function(element){
-
-                var viewport = {
-                    top : $window.pageYOffset,
-                    left : $window.pageXOffset
-                };
-
-                viewport.right = viewport.left + $window.innerWidth;
-                viewport.bottom = viewport.top + $window.innerHeight;
-
-                console.log('viewport', viewport);
-                var bounds = {
-                    top : this.pageYOffset,
-                    left: this.pageXOffset
-                };
+           isOnScreen = function(viewport, element){
                 bounds = element[0].getBoundingClientRect();
-
-                //+200 to slide when in view
-                return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < (bounds.top + 200) || viewport.top > bounds.bottom));
+                return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < (bounds.top + 100) || viewport.top > bounds.bottom));
             };
 
         }
