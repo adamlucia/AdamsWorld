d4c3ef712266ca3a91853a4a0fd691d2788b0fe9
adam <alucia@communityelf.com>
2017-03-15 20:46:03 -0400

    set timeout for 10 sec

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const timeout = require('connect-timeout'); //express v4
 
+//timeout after 10 sec
+app.use(timeout(1000*10));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next){
+    if (!req.timedout){
+        next();
+    } else {
+        console.log('timedout');
+    };
+}
 
 //share the front end files
-app.use("/app", express.static(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, 'app')));
 app.use("/node_modules", express.static(path.join(__dirname, 'node_modules')));
 
+
 app.get('/', function (req, res) {
-    console.log('Sending to index', __dirname + '/index.html');
+    console.log('Sending to index', __dirname + '/app/index.html');
     res.sendFile(__dirname + '/app/index.html');
 });
 app.get('/*', function(req, res) {
