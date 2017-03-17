0cdd4f9c3deb19301daa8da21a0129ca790e1eee
adam <alucia@communityelf.com>
2017-03-17 09:32:22 -0400

    add contact send email function

--- app/components/contact/contact.js
+++ app/components/contact/contact.js
@@ -3,8 +3,23 @@
 angular.module('mySite')
 .component('contact', {
     templateUrl: '/components/contact/contact.html',
-    controller: [function() {
+    controller: ['$http', function($http) {
+        this.test = 'blah';
 
-
+        this.sendInfo = function() {
+            console.log('sending info');
+            $http({
+              method: 'POST',
+              url: '/sendEmail',
+              data: JSON.stringify({'name':'name'})
+            }).then(function successCallback(response) {
+                console.log('sent');
+                // this callback will be called asynchronously
+                // when the response is available
+              }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+              });
+          }
     }]
 });
