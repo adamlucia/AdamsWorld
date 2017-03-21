ebdfc086cce3be4e7fa7e036490f312b68cec466
adam <alucia@communityelf.com>
2017-03-20 21:26:10 -0400

    base smooth scroll

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('mySite', ['ui.bootstrap', 'ui.router', 'ngAnimate'])
-.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $stateProvider, $urlRouterProvider) {
+angular.module('mySite', ['ui.bootstrap', 'ui.router', 'ngAnimate', 'duScroll'])
+.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', '$provide', function($locationProvider, $stateProvider, $urlRouterProvider, $provide) {
     $locationProvider.hashPrefix('!');
     $urlRouterProvider.when('', '/');
 
@@ -32,14 +32,29 @@ angular.module('mySite', ['ui.bootstrap', 'ui.router', 'ngAnimate'])
             }
         }
     })
-    .state('about', {
+    .state('index.about', {
         url: '#about',
     })
-    .state('contact', {
+    .state('index.home.contact', {
         url: '#contact',
+    })
+    .state('index.home.services', {
+        url: '#services',
     });
 
     $urlRouterProvider.otherwise('/');
+
+    $provide.decorator('$uiViewScroll', function ($delegate) {
+        return function (uiViewElement) {
+            console.log('angular.element(document).find', angular.element(document).find('body'));
+            angular.element(document).find('body')[0].animate({
+                scrollTop: uiViewElement[0].offsetTop
+            }, 2000);
+            angular.element(document).find('html')[0].animate({
+                scrollTop: uiViewElement[0].offsetTop
+            }, 2000);
+        };
+    });
 }])
 .run(function(){
     console.log('app running');
